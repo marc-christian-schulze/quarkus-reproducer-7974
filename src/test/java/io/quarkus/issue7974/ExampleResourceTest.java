008340d8c51d8355d0927edbfb29aea9d478a868
Marc-Christian Schulze <Marc.Christian.Schulze@gmail.com>
2020-03-29 18:09:02 +0200

    Properly cleanup subscribers

--- src/test/java/io/quarkus/issue7974/ExampleResourceTest.java
+++ src/test/java/io/quarkus/issue7974/ExampleResourceTest.java
@@ -3,6 +3,8 @@ package io.quarkus.issue7974;
 import io.quarkus.test.junit.QuarkusTest;
 import io.reactivex.subscribers.TestSubscriber;
 import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Publisher;
 
@@ -18,23 +20,28 @@ public class ExampleResourceTest {
     @Inject
     public ExampleResource resource;
 
+    private TestSubscriber<Integer> subscriber;
+
+    @BeforeEach
+    public void setUp() {
+        subscriber = new TestSubscriber<>();
+        publisher.subscribe(subscriber);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        subscriber.dispose();
+    }
+
     @Test
     public void testSenseOfLife() {
-        TestSubscriber<Integer> subscriber = new TestSubscriber<>();
-        publisher.subscribe(subscriber);
-
         resource.emitSenseOfLife();
-
         subscriber.assertValue(42);
     }
 
     @Test
     public void testLuckyNumber() {
-        TestSubscriber<Integer> subscriber = new TestSubscriber<>();
-        publisher.subscribe(subscriber);
-
         resource.emitLuckyNumber();
-
         subscriber.assertValue(13);
     }
 
